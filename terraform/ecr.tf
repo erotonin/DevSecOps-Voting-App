f9f29984a9b767113d9d2fd9ac3150972b010ee6
Hiếu <hieudinhle1204@gmail.com>
2026-04-25 13:20:50 +0700

    feat: complete infrastructure and ci pipeline

---
+++ terraform/ecr.tf
@@ -0,0 +1,23 @@
+resource "aws_ecr_repository" "vote" {
+  name = "voting-app-vote"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "result" {
+  name = "voting-app-result"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "worker" {
+  name = "voting-app-worker"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
