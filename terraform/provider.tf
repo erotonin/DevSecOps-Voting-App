d9d83df67bb55429590049524de0ca7ec19f6f5c
Hiếu <hieudinhle1204@gmail.com>
2026-04-29 10:52:30 +0700

    Refactor: Terraform modules + S3 backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,6 +4,14 @@ terraform {
             source = "hashicorp/aws"
             version = "~> 5.0"
         }
+        helm = {
+            source  = "hashicorp/helm"
+            version = "~> 2.12"
+        }
+        kubernetes = {
+            source  = "hashicorp/kubernetes"
+            version = "~> 2.24"
+        }
     }
 
     backend "s3" {
@@ -17,4 +25,22 @@ terraform {
 
 provider "aws" {
     region = var.aws_region
+}
+
+data "aws_eks_cluster_auth" "main" {
+  name = module.eks.cluster_name
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
+    token                  = data.aws_eks_cluster_auth.main.token
+  }
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
+  token                  = data.aws_eks_cluster_auth.main.token
 }
