3c91ee5360e65d1119f0c0fe0c6e3c378c6159f4
Hiếu <hieudinhle1204@gmail.com>
2026-04-29 17:55:43 +0700

    fix: helm

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -27,20 +27,24 @@ provider "aws" {
     region = var.aws_region
 }
 
-data "aws_eks_cluster_auth" "main" {
-  name = module.eks.cluster_name
-}
-
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
-    token                  = data.aws_eks_cluster_auth.main.token
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.aws_region]
+      command     = "aws"
+    }
   }
 }
 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
-  token                  = data.aws_eks_cluster_auth.main.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.aws_region]
+    command     = "aws"
+  }
 }
